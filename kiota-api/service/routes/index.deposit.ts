d057617a4a7edc9fbaa6be340371711dfbf82d3c
anthony <kimaniantoni@gmail.com>
2026-01-15 16:22:55 +0300

    add middleware requireInternalAuth method to authenticated routes

--- kiota-api/service/routes/index.deposit.ts
+++ kiota-api/service/routes/index.deposit.ts
@@ -1,16 +1,17 @@
 import express from "express";
 import DepositController from "../controllers/deposit.controller";
+import { requireInternalAuth } from "../middleware/auth";
 
 const router = express.Router();
 
 // Deposit flow
-router.post('/initiate', DepositController.initiateDeposit);
-router.post('/trigger-mpesa', DepositController.triggerMpesaPush);
-router.post('/mpesa-callback', DepositController.mpesaCallback);
-router.get('/status/:transactionId', DepositController.getTransactionStatus);
+router.post('/initiate', requireInternalAuth, DepositController.initiateDeposit);
+router.post('/trigger-mpesa', requireInternalAuth, DepositController.triggerMpesaPush);
+router.post('/mpesa-callback', requireInternalAuth, DepositController.mpesaCallback);
+router.get('/status/:transactionId', requireInternalAuth, DepositController.getTransactionStatus);
 
 // Background job endpoint
-router.post('/complete', DepositController.completeDeposit);
+router.post('/complete', requireInternalAuth, DepositController.completeDeposit);
 
 // Catch-all route - must be last
 router.use((req, res) => {
--- kiota-api/service/routes/index.quiz.ts
+++ kiota-api/service/routes/index.quiz.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import QuizController from "../controllers/quiz.controller";
+import { requireInternalAuth } from "../middleware/auth";
 
 const router = express.Router();
 
 // Quiz submission and strategy generation
-router.post('/submit', QuizController.submitQuiz);
-router.post('/accept-strategy', QuizController.acceptStrategy);
+router.post('/submit', requireInternalAuth, QuizController.submitQuiz);
+router.post('/accept-strategy', requireInternalAuth, QuizController.acceptStrategy);
 
 // Get quiz session
-router.get('/latest-session', QuizController.getLatestSession);
+router.get('/latest-session', requireInternalAuth, QuizController.getLatestSession);
 
 // Catch-all route - must be last
 router.use((req, res) => {
