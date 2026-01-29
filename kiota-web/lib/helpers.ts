1ff2a1b0905391082aa74c08147ca7ca74a11ad8
anthony <kimaniantoni@gmail.com>
2026-01-30 02:01:29 +0300

    feat: create reusable onboarding card, completes portfolio onboarding ui

---
+++ kiota-web/lib/helpers.ts
@@ -0,0 +1,5 @@
+export const portfolioOnboardingContent = {
+    title: "Let's set up your Portfolio.",
+    subtitle: "We'll guide you through the steps of setting up your personalized household budget",
+    buttonText: "Get Started"
+}
